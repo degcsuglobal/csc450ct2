34e9a57ad94cafc5533324be711117f91956a7ee
Dan Grover <dan@standardbots.com>
2025-04-26 15:09:54 -0700

    Use the std namespace. Show the loop iteration number in the output.

--- concatstrings.cpp
+++ concatstrings.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char **argv) {
 
 	// We are using the C++ string class, which is dynamically allocated,
 	// to avoid buffer overflows that we might have with char[] arrays, etc. 
-	std::string stringA, stringB;
+	string stringA, stringB;
 
 	// Do this 3 times for varying string lengths (per assignment)
 	for (int i = 0; i < 3; i++) {
-		std::cout << "Enter the first string: ";
-		std::getline(std::cin, stringA);
+		cout << "LOOP ITERATION NUMBER " << i << endl;
 		
-		std::cout << "Enter the second string: ";
-		std::getline(std::cin, stringB);
+		cout << "Enter the first string: ";
+		getline(std::cin, stringA);
 		
-		std::string result = stringA + stringB; // Concatenate the strings!
+		cout << "Enter the second string: ";
+		getline(std::cin, stringB);
+		
+		string result = stringA + stringB; // Concatenate the strings!
 		
 		// Print the result
-		std::cout << "Concatenated result: " << result << std::endl;
+		cout << "Concatenated result: " << result << endl;
 	}
 	
 	return 0;
